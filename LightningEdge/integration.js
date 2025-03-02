b317a95d9e29766dfdb3124866fa130c47ba4ae2
0xAdam <159015291+vininha-carvalho@users.noreply.github.com>
2025-03-02 05:50:13 +0200

    Delete LightningEdge directory

--- LightningEdge/integration.js
+++
@@ -1,3 +0,0 @@
-const { Connection } = require("@solana/web3.js");
-const customRpcUrl = "https://lb.drpc.org/ogrpc?network=solana&dkey=Arc_JqtwaUlmmje2rvgtJWyamxyDxxAR77DXIlZWwHzRp:8899"; // https://lb.drpc.org/ogrpc?network=solana&dkey=Arc_JqtwaUlmmje2rvgtJWyamxyDxxAR77DXIlZWwHzR
-const connection = new Connection(customRpcUrl, "confirmed");
